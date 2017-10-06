8e1ef3c70bbfab2713732d7eb1e32026312fc239
vishalkaneria1987 <vishalkaneria1987@gmail.com>
2017-10-06 15:47:08 +0530

    Add project files.

---
+++ DatabaseVAlidation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DatabaseVAlidation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DatabaseVAlidation")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1309cac8-24ad-4d85-8a29-eda3f4d8a03c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DatabaseVAlidation/UnitTest1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Data.SqlClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DatabaseVAlidation
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+           // Console.WriteLine("Hello");
+            //string connetionString = null;
+            SqlDataReader rdr = null;
+
+            SqlConnection conn = new SqlConnection("Data Source=CGBD014894\\QGBD019003;Initial Catalog=LoadTestUAT;Integrated Security=TRUE");
+            SqlCommand cmd = new SqlCommand("select * from LoadTestNetworks", conn);
+
+            try
+            {
+
+                // open the connection
+                conn.Open();
+
+                // 1. get an instance of the SqlDataReader
+                rdr = cmd.ExecuteReader();
+
+                // print a set of column headers
+                Console.WriteLine(" LoadTestRunId   NetworkId   NetworkName");
+                Console.WriteLine("---------------  ------------  ------------ ");
+
+                // 2. print necessary columns of each  record
+
+                while (rdr.Read())
+                {
+                    // get the results of each column
+                     string ID = rdr["LoadTestRunId"].ToString();
+                    string NetworkId = rdr["NetworkId"].ToString();
+                    string NetworkName = rdr["NetworkName"].ToString();
+
+                    // print out the results
+                    //Console.Write("{0,-25}", ID);
+                    //Console.Write(ID);
+                    Console.Write("{0,-25}", ID);
+                    Console.Write("{0,-25}", NetworkId);
+                    Console.Write("{0,-25}", NetworkName);
+                    Console.WriteLine();
+                }
+            }
+            finally
+            {
+                // 3. close the reader
+                if (rdr != null)
+                {
+                    rdr.Close();
+                }
+
+                // close the connection
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
+
