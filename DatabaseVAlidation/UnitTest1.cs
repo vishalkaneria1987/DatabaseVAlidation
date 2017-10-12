9247bcda0b213079eaa1dc6df6560206620b657e
vishalkaneria1987 <vishalkaneria1987@gmail.com>
2017-10-12 14:31:15 +0530

    Vishal Commit Comot

--- DatabaseVAlidation/UnitTest1.cs
+++ DatabaseVAlidation/UnitTest1.cs
@@ -10,59 +10,61 @@ namespace DatabaseVAlidation
         [TestMethod]
         public void TestMethod1()
         {
-           // Console.WriteLine("Hello");
-            //string connetionString = null;
-            SqlDataReader rdr = null;
+              // Console.WriteLine("Hello");
+               //string connetionString = null;
+               SqlDataReader rdr = null;
 
-            SqlConnection conn = new SqlConnection("Data Source=CGBD014894\\QGBD019003;Initial Catalog=LoadTestUAT;Integrated Security=TRUE");
-            SqlCommand cmd = new SqlCommand("select * from LoadTestNetworks", conn);
+               SqlConnection conn = new SqlConnection("Data Source=CGBD014894\\QGBD019003;Initial Catalog=LoadTestUAT;Integrated Security=TRUE");
+               SqlCommand cmd = new SqlCommand("select * from LoadTestNetworks", conn);
 
-            try
-            {
+               try
+               {
 
-                // open the connection
-                conn.Open();
+                   // open the connection
+                   conn.Open();
 
-                // 1. get an instance of the SqlDataReader
-                rdr = cmd.ExecuteReader();
+                   // 1. get an instance of the SqlDataReader
+                   rdr = cmd.ExecuteReader();
 
-                // print a set of column headers
-                Console.WriteLine(" LoadTestRunId   NetworkId   NetworkName");
-                Console.WriteLine("---------------  ------------  ---------- ");
+                   // print a set of column headers
+                   Console.WriteLine(" LoadTestRunId   NetworkId   NetworkName");
+                   Console.WriteLine("---------------  ------------  ---------- ");
 
-                // 2. print necessary columns of each  record
+                   // 2. print necessary columns of each  record
 
-                while (rdr.Read())
-                {
-                    // get the results of each column
-                     string ID = rdr["LoadTestRunId"].ToString();
-                    string NetworkId = rdr["NetworkId"].ToString();
-                    string NetworkName = rdr["NetworkName"].ToString();
+                   while (rdr.Read())
+                   {
+                       // get the results of each column
+                        string ID = rdr["LoadTestRunId"].ToString();
+                       string NetworkId = rdr["NetworkId"].ToString();
+                       string NetworkName = rdr["NetworkName"].ToString();
 
-                    // print out the results
-                    //Console.Write("{0,-25}", ID);
-                    //Console.Write(ID);
-                    Console.Write("{0,-25}", ID);
-                    Console.Write("{0,-25}", NetworkId);
-                    Console.Write("{0,-25}", NetworkName);
-                    Console.WriteLine();
-                }
-            }
-            finally
-            {
-                // 3. close the reader
-                if (rdr != null)
-                {
-                    rdr.Close();
-                }
+                       // print out the results
+                       //Console.Write("{0,-25}", ID);
+                       //Console.Write(ID);
+                       Console.Write("{0,-25}", ID);
+                       Console.Write("{0,-25}", NetworkId);
+                       Console.Write("{0,-25}", NetworkName);
+                       Console.WriteLine();
+                   }
+               }
+               finally
+               {
+                   // 3. close the reader
+                   if (rdr != null)
+                   {
+                       rdr.Close();
+                   }
 
-                // close the connection
-                if (conn != null)
-                {
-                    conn.Close();
-                }
-            }
-        }
-    }
-}
+                   // close the connection
+                   if (conn != null)
+                   {
+                       conn.Close();
+                   }
+               }
+           }
+       }
+   } 
 
+
+  
